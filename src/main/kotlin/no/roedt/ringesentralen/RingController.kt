a482bc18578f721300b3ea1cb1dbc4648fd140d8
Mads Opheim <mads.opheim@gmail.com>
2020-10-03 18:00:50 +0200

    #2: Path og litt openapi-skildring

--- src/main/kotlin/no/roedt/ringesentralen/RingController.kt
+++ src/main/kotlin/no/roedt/ringesentralen/RingController.kt
@@ -1,5 +1,6 @@
 package no.roedt.ringesentralen
 
+import org.eclipse.microprofile.openapi.annotations.Operation
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
@@ -9,6 +10,8 @@ class RingController(val ringService: RingService) {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/neste")
+    @Operation(summary = "Finn neste person å ringe")
     fun hentNestePersonAaRinge(@QueryParam("lokallagID") nestePersonAaRingeRequest: Int): RingbarPerson? = ringService.hentNestePersonAaRinge(nestePersonAaRingeRequest)
 
 }
