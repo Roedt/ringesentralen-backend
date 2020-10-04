047489649b7eeb4ff64f5c8338c127efea87fc89
Mads Opheim <mads.opheim@gmail.com>
2020-10-04 12:07:37 +0200

    #1: Liveness-probe. Readiness fins allereie bra konfigurert i Quarkus

---
+++ src/main/kotlin/no/roedt/ringesentralen/health/LIvenessCheck.kt
@@ -0,0 +1,14 @@
+package no.roedt.ringesentralen.health
+
+import org.eclipse.microprofile.health.HealthCheck
+import org.eclipse.microprofile.health.HealthCheckResponse
+import org.eclipse.microprofile.health.Liveness
+import javax.enterprise.context.ApplicationScoped
+
+@Liveness
+@ApplicationScoped
+class LivenessCheck : HealthCheck {
+    override fun call(): HealthCheckResponse = HealthCheckResponse.up(LivenessCheck::class.simpleName)
+
+
+}
