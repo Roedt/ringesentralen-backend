9e75ac4badad608f2e26fbf20d569e5f7eb23f8e
Mads Opheim <mads.opheim@gmail.com>
2020-10-22 23:17:37 +0200

    #7: Trekkjer ut service-klasse

--- src/main/kotlin/no/roedt/ringesentralen/hypersys/HypersysEndpoint.kt
+++ src/main/kotlin/no/roedt/ringesentralen/hypersys/HypersysEndpoint.kt
@@ -1,19 +1,6 @@
 package no.roedt.ringesentralen.hypersys
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.readValue
-import org.apache.http.client.methods.CloseableHttpResponse
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpUriRequest
-import org.apache.http.entity.StringEntity
-import org.apache.http.impl.client.HttpClientBuilder
-import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
-import java.util.*
-import javax.annotation.PostConstruct
 import javax.enterprise.context.ApplicationScoped
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -23,77 +10,21 @@ import javax.ws.rs.core.MediaType
 @ApplicationScoped
 @Path("/hypersys")
 @Tag(name = "Hypersys-integrasjon")
-class HypersysEndpoint {
-    @ConfigProperty(name = "baseURL")
-    lateinit var baseURL: String
-
-    @ConfigProperty(name = "clientId")
-    lateinit var clientId: String   
-    
-    @ConfigProperty(name = "clientSecret")
-    lateinit var clientSecret: String
-
-    lateinit var token: Token
-    
-    val kMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-
-    @PostConstruct
-    fun fetchToken() {
-        token = getTokenFromHypersys()
-    }
+class HypersysEndpoint(val service: HypersysService) {
 
     @GET
     @Path("/token")
     @Produces(MediaType.APPLICATION_JSON)
-    fun getTokenFromHypersys(): Token {
-        val base64Credentials: String = Base64.getEncoder().encodeToString(("$clientId:$clientSecret").toByteArray())
-        val httpPost = HttpPost("$baseURL/api/o/token/")
-        httpPost.addHeader("Authorization", "Basic $base64Credentials")
-        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded")
-        httpPost.entity = StringEntity("grant_type=client_credentials")
-        val response = httpCall(httpPost)
-        assert(response?.statusLine?.statusCode == 200)
-        return readResponse(response)
-    }
+    fun getTokenFromHypersys(): Token = service.getTokenFromHypersys()
 
     @GET
     @Path("/lokallag")
     @Produces(MediaType.APPLICATION_JSON)
-    fun getAlleLokallag() : List<Organisasjonsledd> = readResponse(gjennomfoerGetkall("/org/api/"))
+    fun getAlleLokallag() : List<Organisasjonsledd> = service.getAlleLokallag()
 
     @GET
     @Path("/alleOrgan")
     @Produces(MediaType.APPLICATION_JSON)
-    fun getAlleOrganPaaLaagasteNivaa(): List<SingleOrgan> = getAlleLokallag().map { toSingleOrgans(it) }.flatten()
+    fun getAlleOrganPaaLaagasteNivaa(): List<SingleOrgan> = service.getAlleOrganPaaLaagasteNivaa()
 
-    private fun toSingleOrgans(lokallag: Organisasjonsledd): List<SingleOrgan> {
-        val organs: Organs = readResponse(gjennomfoerGetkall("org/api/${lokallag.id}/organ"))
-        return organs.organs.map {
-            readResponse<SingleOrgan>(gjennomfoerGetkall("org/api/${lokallag.id}/organ/${it.id}"))
-        }
-    }
-
-    private fun gjennomfoerGetkall(url: String): CloseableHttpResponse? {
-        ensureTokenIsValid()
-        val httpGet = HttpGet("$baseURL/$url")
-        httpGet.addHeader("Authorization", "Bearer ${token.access_token}")
-        val response = httpCall(httpGet)
-        assert(response?.statusLine?.statusCode == 200)
-        return response
-    }
-
-    private inline fun <reified T> readResponse(response: CloseableHttpResponse?) =
-            response?.entity?.content
-                    ?.bufferedReader()
-                    .use { it?.readText() }
-                    ?.let<String, T> { kMapper.readValue(it)}!!
-
-    private fun ensureTokenIsValid() {
-        if (token.expires_in <= 0) {
-            fetchToken()
-        }
-    }
-
-    fun httpCall(request: HttpUriRequest) = HttpClientBuilder.create().build()?.execute(request)
 }
---
+++ src/main/kotlin/no/roedt/ringesentralen/hypersys/HypersysService.kt
@@ -0,0 +1,90 @@
+package no.roedt.ringesentralen.hypersys
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.util.*
+import javax.annotation.PostConstruct
+import javax.enterprise.context.ApplicationScoped
+
+interface HypersysService {
+    fun getTokenFromHypersys(): Token
+    fun getAlleLokallag(): List<Organisasjonsledd>
+    fun getAlleOrganPaaLaagasteNivaa(): List<SingleOrgan>
+}
+
+@ApplicationScoped
+class HypersysServiceBean : HypersysService {
+    @ConfigProperty(name = "baseURL")
+    lateinit var baseURL: String
+
+    @ConfigProperty(name = "clientId")
+    lateinit var clientId: String
+
+    @ConfigProperty(name = "clientSecret")
+    lateinit var clientSecret: String
+
+    lateinit var token: Token
+
+    val kMapper: ObjectMapper = ObjectMapper().registerModule(KotlinModule())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    @PostConstruct
+    fun fetchToken() {
+        token = getTokenFromHypersys()
+    }
+
+    override fun getTokenFromHypersys(): Token  {
+        val base64Credentials: String = Base64.getEncoder().encodeToString(("$clientId:$clientSecret").toByteArray())
+        val httpPost = HttpPost("$baseURL/api/o/token/")
+        httpPost.addHeader("Authorization", "Basic $base64Credentials")
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded")
+        httpPost.entity = StringEntity("grant_type=client_credentials")
+        val response = httpCall(httpPost)
+        assert(response?.statusLine?.statusCode == 200)
+        return readResponse(response)
+    }
+
+    override fun getAlleLokallag(): List<Organisasjonsledd> = readResponse(gjennomfoerGetkall("/org/api/"))
+
+    override fun getAlleOrganPaaLaagasteNivaa(): List<SingleOrgan> = getAlleLokallag().map { toSingleOrgans(it) }.flatten()
+
+    private fun toSingleOrgans(lokallag: Organisasjonsledd): List<SingleOrgan> {
+        val organs: Organs = readResponse(gjennomfoerGetkall("org/api/${lokallag.id}/organ"))
+        return organs.organs.map {
+            readResponse<SingleOrgan>(gjennomfoerGetkall("org/api/${lokallag.id}/organ/${it.id}"))
+        }
+    }
+
+    private fun gjennomfoerGetkall(url: String): CloseableHttpResponse? {
+        ensureTokenIsValid()
+        val httpGet = HttpGet("$baseURL/$url")
+        httpGet.addHeader("Authorization", "Bearer ${token.access_token}")
+        val response = httpCall(httpGet)
+        assert(response?.statusLine?.statusCode == 200)
+        return response
+    }
+
+    private inline fun <reified T> readResponse(response: CloseableHttpResponse?) =
+            response?.entity?.content
+                    ?.bufferedReader()
+                    .use { it?.readText() }
+                    ?.let<String, T> { kMapper.readValue(it)}!!
+
+    private fun ensureTokenIsValid() {
+        if (token.expires_in <= 0) {
+            fetchToken()
+        }
+    }
+    fun httpCall(request: HttpUriRequest) = HttpClientBuilder.create().build()?.execute(request)
+
+
+}
