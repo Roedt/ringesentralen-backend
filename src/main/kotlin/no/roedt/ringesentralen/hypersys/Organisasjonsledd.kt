ad05adf7d765a13197e6a510c3f500e1231ee2b8
Mads Opheim <mads.opheim@gmail.com>
2020-10-22 21:53:30 +0200

    #7: Kode for å hente alle lokallag frå Hypersys

---
+++ src/main/kotlin/no/roedt/ringesentralen/hypersys/Organisasjonsledd.kt
@@ -0,0 +1,14 @@
+package no.roedt.ringesentralen.hypersys
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Organisasjonsledd(
+        @JsonProperty("id") val id: Int,
+        @JsonProperty("name") val name: String,
+        @JsonProperty("organisation_number") val organisation_number: String?,
+        @JsonProperty("type_name") val type_name: String?,
+        @JsonProperty("parent") val parent: Int?,
+        @JsonProperty("parent_name") val parent_name: String?,
+        @JsonProperty("children") val children: Array<Int>?,
+        @JsonProperty("status") val status: Int
+)
