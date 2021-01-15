40db75fcebf8089db86d54d0494d33b4a96d1a7f
Mads Opheim <mads@fostad.net>
2021-01-15 23:37:39 +0100

    #14: Forenklar, og kuttar ned igjen unødig lang token-tid

--- src/main/kotlin/no/roedt/ringesentralen/token/TokenGenerator.kt
+++ src/main/kotlin/no/roedt/ringesentralen/token/TokenGenerator.kt
@@ -1,20 +1,11 @@
 package no.roedt.ringesentralen.token
 
 import io.smallrye.jwt.build.Jwt
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.time.Duration
-import javax.annotation.PostConstruct
 import javax.enterprise.context.RequestScoped
 
 @RequestScoped
 class TokenGenerator {
-    private var key: String? = null
-
-    @PostConstruct
-    fun init() {
-        key = Files.readString(Paths.get("../src/main/resources/META-INF/resources/privatekey.pem"))
-    }
 
     fun generateToken(): String = Jwt
         .audience("ringar")
@@ -22,7 +13,7 @@ class TokenGenerator {
         .subject("Ringesentralen")
         .upn("Ringesentralen")
         .issuedAt(System.currentTimeMillis())
-        .expiresAt(System.currentTimeMillis() + Duration.ofMinutes(30).toSeconds())
+        .expiresAt(System.currentTimeMillis() + Duration.ofMinutes(1).toSeconds())
         .groups("ringar")
         .sign("META-INF/resources/privatekey.pem")
 }
