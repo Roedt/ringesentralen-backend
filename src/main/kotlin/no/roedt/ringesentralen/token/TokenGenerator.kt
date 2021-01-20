405c90ff97bbfe40854b5d86d20305f51a2f3245
Mads Opheim <mads@fostad.net>
2021-01-20 23:37:56 +0100

    #21: Koplar opp mot GCP Secret Manager for å hente privatekey derifrå i prod

--- src/main/kotlin/no/roedt/ringesentralen/token/TokenGenerator.kt
+++ src/main/kotlin/no/roedt/ringesentralen/token/TokenGenerator.kt
@@ -1,8 +1,12 @@
 package no.roedt.ringesentralen.token
 
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient
+import com.google.cloud.secretmanager.v1.SecretVersionName
 import io.smallrye.jwt.build.Jwt
 import no.roedt.ringesentralen.hypersys.*
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.nio.file.Files
+import java.nio.file.Path
 import java.time.Duration
 import javax.enterprise.context.RequestScoped
 
@@ -11,6 +15,15 @@ class TokenGenerator(val hypersysService: HypersysService) {
 
     @ConfigProperty(name = "frontendTokenKey")
     lateinit var frontendTokenKey: String
+
+    @ConfigProperty(name = "usePrivateKeyFromSecretManager", defaultValue = "false")
+    lateinit var usePrivateKeyFromSecretManager: String
+
+    @ConfigProperty(name = "secretManagerProjectId", defaultValue = "")
+    lateinit var secretManagerProjectId: String
+
+    @ConfigProperty(name = "secretManagerSecretName", defaultValue = "")
+    lateinit var secretManagerSecretName: String
 
     fun login(loginRequest: LoginRequest): String {
         if (loginRequest.key != frontendTokenKey) {
@@ -35,5 +48,15 @@ class TokenGenerator(val hypersysService: HypersysService) {
         .claim("hypersys.scope", hypersysToken.scope)
         .claim("hypersys.access_token", hypersysToken.access_token)
         .claim("hypersys.expires_in", hypersysToken.expires_in)
-        .sign("META-INF/resources/privatekey.pem")
+        .signWithSecret(getPrivateKey())
+
+    private fun getPrivateKey(): String =
+        if (usePrivateKeyFromSecretManager.toBoolean()) getPrivateKeyFromSecretManager()
+        else Files.readString(Path.of("../src/main/resources/META-INF/resources/privatekey.pem"))
+
+    private fun getPrivateKeyFromSecretManager(): String {
+        val client = SecretManagerServiceClient.create()
+        val secretVersionName = SecretVersionName.of(secretManagerProjectId, secretManagerSecretName, "latest")
+        return client.accessSecretVersion(secretVersionName).payload.data.toStringUtf8()
+    }
 }
