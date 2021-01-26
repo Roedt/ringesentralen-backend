9fa273d5cc9aab2823264f6e526344848c0fa4af
Mads Opheim <mads@fostad.net>
2021-01-26 23:42:44 +0100

    #8: Gløymte registerforreflection på ei dataklasse

--- src/main/kotlin/no/roedt/ringesentralen/statistikk/Resultattype.kt
+++ src/main/kotlin/no/roedt/ringesentralen/statistikk/Resultattype.kt
@@ -1,3 +1,6 @@
 package no.roedt.ringesentralen.statistikk
 
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@RegisterForReflection
 data class Resultattype(val id: Int, val displaytext: String)
