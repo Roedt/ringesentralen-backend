3b2e6e345dfd596359ba3feda02fc06521718464
Mads Opheim <mads@fostad.net>
2021-01-26 21:09:57 +0100

    #34: Legg på registerforreflection på nye dataklassar

--- src/main/kotlin/no/roedt/ringesentralen/brukere/AutentisertTilgangsendringRequest.kt
+++ src/main/kotlin/no/roedt/ringesentralen/brukere/AutentisertTilgangsendringRequest.kt
@@ -1,8 +1,9 @@
 package no.roedt.ringesentralen.brukere
 
 import UserId
+import io.quarkus.runtime.annotations.RegisterForReflection
 
+@RegisterForReflection
 data class AutentisertTilgangsendringRequest(val userId: UserId, val tilgangsendringRequest: TilgangsendringsRequest) {
-
     fun personMedEndraTilgang() = tilgangsendringRequest.personMedEndraTilgang
 }
