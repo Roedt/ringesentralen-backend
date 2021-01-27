e3a51b17aa58d258cf2732ef3116124119748c63
Mads Opheim <mads@fostad.net>
2021-01-27 23:04:14 +0100

    Test som følgjer heile flyten. Foreløpig utkommentert som JUnit-test
    fordi credentials

---
+++ src/test/kotlin/FullFlytTest.kt
@@ -0,0 +1,67 @@
+
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import io.restassured.response.ValidatableResponseOptions
+import no.roedt.ringesentralen.Modus
+import no.roedt.ringesentralen.hypersys.LoginRequest
+import no.roedt.ringesentralen.samtale.*
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.jupiter.api.Test
+
+class FullFlytTest {
+
+//    @Test
+    fun test() {
+        val token = login()
+
+        val ringtId: Long = 123452
+        loginWithToken(token)
+            .body(StartSamtaleRequest(skalRingesID = ringtId))
+            .contentType(ContentType.JSON)
+            .post("/samtale/startSamtale")
+            .then()
+            .statusCode(200)
+
+        loginWithToken(token)
+            .body(ResultatFraSamtaleRequest(
+                modus = Modus.Korona,
+                ringtID = ringtId,
+                result = Resultat.Passet_ikke,
+                kommentar = "Frå automatisk test",
+                modusspesifikkeResultat = KoronaspesifikkeResultat(
+                    vilBliMerAktiv = true,
+                    vilHaKoronaprogram = true,
+                    vilHaValgkampsbrev = false
+                ),
+                vilIkkeBliRingt = false)
+            )
+            .contentType(ContentType.JSON)
+            .post("/samtale/registrerResultatFraSamtale")
+            .then()
+            .statusCode(200)
+
+        loginWithToken(token)
+            .body(RingerTilbakeRequest(ringtNummer = "12345678"))
+            .contentType(ContentType.JSON)
+            .post("/samtale/noenRingerTilbake")
+            .then()
+            .statusCode(200)
+            .body("givenName", equalTo("Donald"))
+    }
+
+    private fun login(): String {
+        val loginRequest = LoginRequest(brukarnamn = "dittBrukarnamn", key = "dinKey", passord = "dittPassord")
+        val response = given()
+            .body(loginRequest)
+            .contentType(ContentType.JSON)
+            .`when`()
+            .post("/token/login")
+            .then()
+            .statusCode(200)
+        return response.responseAsString()
+    }
+
+    private fun loginWithToken(token: String) = given().`when`().header("Authorization", "Bearer $token")
+
+    private fun Any.responseAsString() =(this as ValidatableResponseOptions<*, *>).log().all().extract().response().asString()
+}
