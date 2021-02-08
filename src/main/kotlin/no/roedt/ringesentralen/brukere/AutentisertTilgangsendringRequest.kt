c80d9e4404c152a80aaf88ab80f6a1fa6a6b353d
Mads Opheim <mads@fostad.net>
2021-02-08 22:21:07 +0100

    #72: Flyttar autentisert-requests inn i samme filer som requestane

--- src/main/kotlin/no/roedt/ringesentralen/brukere/AutentisertTilgangsendringRequest.kt
+++
@@ -1,9 +0,0 @@
-package no.roedt.ringesentralen.brukere
-
-import no.roedt.ringesentralen.UserId
-import io.quarkus.runtime.annotations.RegisterForReflection
-
-@RegisterForReflection
-data class AutentisertTilgangsendringRequest(val userId: UserId, val tilgangsendringRequest: TilgangsendringsRequest) {
-    fun personMedEndraTilgang() = tilgangsendringRequest.personMedEndraTilgang
-}
--- src/main/kotlin/no/roedt/ringesentralen/brukere/TilgangsendringsRequest.kt
+++ src/main/kotlin/no/roedt/ringesentralen/brukere/TilgangsendringsRequest.kt
@@ -2,8 +2,14 @@ package no.roedt.ringesentralen.brukere
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.quarkus.runtime.annotations.RegisterForReflection
+import no.roedt.ringesentralen.UserId
 
 @RegisterForReflection
 data class TilgangsendringsRequest(
         @JsonProperty("personMedEndraTilgang") val personMedEndraTilgang: Long
 )
+
+@RegisterForReflection
+data class AutentisertTilgangsendringRequest(val userId: UserId, val tilgangsendringRequest: TilgangsendringsRequest) {
+        fun personMedEndraTilgang() = tilgangsendringRequest.personMedEndraTilgang
+}
