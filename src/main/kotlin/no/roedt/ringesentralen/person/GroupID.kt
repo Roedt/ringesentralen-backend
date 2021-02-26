7f99f3c9ea63f7761e49b54c7938bcda00e6f583
Mads Opheim <mads@fostad.net>
2021-02-26 19:55:01 +0100

    #98: Flyttar statisk metode inn i enum

--- src/main/kotlin/no/roedt/ringesentralen/person/GroupID.kt
+++ src/main/kotlin/no/roedt/ringesentralen/person/GroupID.kt
@@ -2,31 +2,23 @@ package no.roedt.ringesentralen.person
 
 import no.roedt.ringesentralen.Roles
 
-enum class GroupID(val nr: Int, val skildring: String) {
-    ManglerSmtykke(0, "mangler info/samtykke før ringing"),
-    KlarTilAaRinges(1, "klar til å ringes"),
-    Ferdigringt(2, "ferdigringt"),
-    Slett(3, "slett"),
-    UgodkjentRinger(4, "ugodkjent ringer"),
-    AvslaattRinger(5, "ringer som aktivt ikke er godkjent"),
-    GodkjentRinger(6, "godkjent ringer og relay-bruker"),
-    TrengerOppfoelging(7, "trenger oppfølging"),
-    LokalGodkjenner(8, "ringer som kan godkjenne ringere i sitt lokallag"),
-    Admin(9, "admin");
+enum class GroupID(val nr: Int, val skildring: String, val roller: Set<String>) {
+    ManglerSmtykke(0, "mangler info/samtykke før ringing", setOf()),
+    KlarTilAaRinges(1, "klar til å ringes", setOf()),
+    Ferdigringt(2, "ferdigringt", setOf()),
+    Slett(3, "slett", setOf()),
+    UgodkjentRinger(4, "ugodkjent ringer", setOf(Roles.venterPaaGodkjenning)),
+    AvslaattRinger(5, "ringer som aktivt ikke er godkjent", setOf(Roles.sperret)),
+    GodkjentRinger(6, "godkjent ringer og relay-bruker", setOf(Roles.bruker, Roles.ringer)),
+    TrengerOppfoelging(7, "trenger oppfølging", setOf()),
+    LokalGodkjenner(8, "ringer som kan godkjenne ringere i sitt lokallag", setOf(Roles.bruker, Roles.ringer, Roles.godkjenner)),
+    Admin(9, "admin", setOf(Roles.bruker, Roles.ringer, Roles.godkjenner, Roles.admin));
 
     fun references(value: Int): Boolean = nr == value
 
     companion object {
         fun from(value: Int): GroupID = values().first { it.nr == value }
 
-        fun getRoller(groupID: Int) =
-            when (from(groupID)) {
-                Admin -> setOf(Roles.bruker, Roles.ringer, Roles.godkjenner, Roles.admin)
-                LokalGodkjenner -> setOf(Roles.bruker, Roles.ringer, Roles.godkjenner)
-                GodkjentRinger -> setOf(Roles.bruker, Roles.ringer)
-                UgodkjentRinger -> setOf(Roles.venterPaaGodkjenning)
-                AvslaattRinger -> setOf(Roles.sperret)
-                else -> setOf()
-            }
+        fun getRoller(groupID: Int) = from(groupID).roller
     }
 }
