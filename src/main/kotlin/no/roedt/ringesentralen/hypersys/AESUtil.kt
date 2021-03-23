bb3510b7ce2978b8259dd42d1786acc62e6eabde
Mads Opheim <mads@fostad.net>
2021-03-23 20:30:24 +0100

    #104: AES-CBC-256-implementasjon
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/kotlin/no/roedt/ringesentralen/hypersys/AESUtil.kt
+++ src/main/kotlin/no/roedt/ringesentralen/hypersys/AESUtil.kt
@@ -1,5 +1,6 @@
 package no.roedt.ringesentralen.hypersys
 
+import no.roedt.ringesentralen.token.SecretFactory
 import java.security.MessageDigest
 import java.security.SecureRandom
 import java.util.*
@@ -9,21 +10,23 @@ import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-class AESUtil {
+class AESUtil(val secretFactory: SecretFactory) {
 
     private val algorithm: String = "AES/CBC/PKCS5Padding"
+    private val charset = Charsets.UTF_8
     lateinit var key: SecretKey
 
     @PostConstruct
     fun setKey() {
-        val md = MessageDigest.getInstance("SHA-256")
-        key = SecretKeySpec(md.digest("Ek6MKdSdEz1EiRs6cX3uUUAAnMYua17h".toByteArray(Charsets.UTF_8)), "AES")
+        key = SecretKeySpec(MessageDigest.getInstance("SHA-256").digest(secretFactory.getEncryptionKey().toByteArray(
+            charset
+        )), "AES")
     }
 
     fun encrypt(input: String, iv: IvParameterSpec?): String {
         val cipher = Cipher.getInstance(algorithm)
         cipher.init(Cipher.ENCRYPT_MODE, key, iv)
-        val cipherText = cipher.doFinal(input.toByteArray())
+        val cipherText = cipher.doFinal(input.toByteArray(charset))
         return Base64.getEncoder().encodeToString(cipherText)
     }
 
@@ -33,19 +36,10 @@ class AESUtil {
         val plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText))
         return String(plainText)
     }
+
     fun generateIv(): IvParameterSpec {
         val iv = ByteArray(16)
         SecureRandom().nextBytes(iv)
         return IvParameterSpec(iv)
     }
-
 }
-
-//fun getKeyFromPassword(password: String, salt: String): SecretKey {
-//    val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
-//    val spec: KeySpec = PBEKeySpec(password.toCharArray(), salt.toByteArray(), 65536, 256)
-//    return SecretKeySpec(
-//        factory.generateSecret(spec)
-//            .encoded, "AES"
-//    )
-//}
--- src/test/kotlin/no/roedt/ringesentralen/hypersys/AESUtilsTest.kt
+++ src/test/kotlin/no/roedt/ringesentralen/hypersys/AESUtilsTest.kt
@@ -1,16 +1,25 @@
 package no.roedt.ringesentralen.hypersys
 
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import no.roedt.ringesentralen.token.SecretFactory
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class AESUtilUnitTest {
+
+    private val secretFactory: SecretFactory = mock()
+
     @Test
     fun givenString_whenEncrypt_thenSuccess() {
         val kryptertFraFrontend = "bdb191129ea6082a6adecd62f780f943:72b5cdf548bef46534103c45f15a3198335ae5b6fc34583a080a0cb0916fc8b5"
+        val keyString = "Ek6MKdSdEz1EiRs6cX3uUUAAnMYua17h"
+        doReturn(keyString).whenever(secretFactory).getEncryptionKey()
 
         val input = "dette er en veldig fin tekst"
 
-        val aesUtil = AESUtil()
+        val aesUtil = AESUtil(secretFactory)
         aesUtil.setKey()
 
         val ivParameterSpec = aesUtil.generateIv()
