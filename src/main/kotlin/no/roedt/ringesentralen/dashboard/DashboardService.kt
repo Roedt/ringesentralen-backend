e422606972dc1e61413dcccadd85d2f7e37fd77a
Mads Opheim <mads@fostad.net>
2021-03-28 14:26:41 +0200

    Fixes #160: Bruker enumverdiar heller enn magiske tal
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/kotlin/no/roedt/ringesentralen/dashboard/DashboardService.kt
+++ src/main/kotlin/no/roedt/ringesentralen/dashboard/DashboardService.kt
@@ -26,9 +26,9 @@ class DashboardServiceBean(
 
         val statusliste: List<Lokallagsstatus> = mineLokallag
                 .map { lokallag ->
-                    val igjenAaRinge = personRepository.list("groupID<2 and lokallag=?1", lokallag.id.toInt()).size
+                    val igjenAaRinge = personRepository.list("groupID=?1 and lokallag=?2", GroupID.KlarTilAaRinges.nr, lokallag.id.toInt()).size
                     val personerSomKanRinges = databaseUpdater.getResultList("SELECT 1 FROM v_personerSomKanRinges WHERE lokallag = ${lokallag.id}").size
-                    val totaltInklRingte = personRepository.list("lokallag=?1 and groupID=2", lokallag.id.toInt()).size
+                    val totaltInklRingte = personRepository.list("lokallag=?1 and groupID=?2", lokallag.id.toInt(), GroupID.Ferdigringt.nr).size
                     Lokallagsstatus(
                             lokallag = lokallag,
                             igjenAaRinge = igjenAaRinge,
