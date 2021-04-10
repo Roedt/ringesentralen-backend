93839c2002f4e2b74712ea6b04231d7163bdf9a6
Mads Opheim <mads@fostad.net>
2021-04-10 19:25:41 +0200

    Fixes #195: Går tilbake til å bruke plain SQL for statistikk-sida, det er mykje raskare
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkService.kt
+++ src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkService.kt
@@ -2,20 +2,12 @@ package no.roedt.ringesentralen.statistikk
 
 import no.roedt.ringesentralen.DatabaseUpdater
 import no.roedt.ringesentralen.Roles
-import no.roedt.ringesentralen.hypersys.RingerRepository
 import no.roedt.ringesentralen.person.GroupID
-import no.roedt.ringesentralen.person.PersonRepository
-import no.roedt.ringesentralen.samtale.PersistentSamtaleRepository
 import no.roedt.ringesentralen.samtale.resultat.Resultat
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class StatistikkService(
-    val databaseUpdater: DatabaseUpdater,
-    val ringerRepository: RingerRepository,
-    val personRepository: PersonRepository,
-    val samtaleRepository: PersistentSamtaleRepository
-) {
+class StatistikkService(val databaseUpdater: DatabaseUpdater) {
 
     fun getStatistikk(groups: Set<String>): StatistikkResponse {
         return if (groups.contains(Roles.admin)) {
@@ -37,25 +29,25 @@ class StatistikkService(
             .map {
                 SamtaleResultat(
                     displaytext = it.displaytext,
-                    antal = samtaleRepository.list("resultat=?1", it.id).map { samtale -> samtale.ringer }.count()
+                    antal = get("SELECT ringer FROM `samtale` WHERE resultat = ${it.id}").size
                 )
             }
             .filter { it.antal > 0}
 
         return SamtalerStatistikkResponse(
             resultat = list,
-            samtalerMedResultatSaaLangt = samtaleRepository.count("resultat!=${Resultat.Samtale_startet.nr}").toInt()
+            samtalerMedResultatSaaLangt = get("SELECT ringer FROM `samtale` WHERE resultat!=${Resultat.Samtale_startet.nr}").size
         )
     }
 
     private fun getRingereStatistikkResponse(): RingereStatistikkResponse =
         RingereStatistikkResponse(
-            registrerteRingere = ringerRepository.count().toInt(),
+            registrerteRingere = get("SELECT 1 FROM ringer").size,
             antallSomHarRingt = get("select distinct ringer from `samtale`").size,
             aktiveRingereDenSisteTimen = get("select distinct ringer from `samtale` where UNIX_TIMESTAMP(now()) - unix_timestamp(datetime) < 3600").size,
             aktiveRingereIDag = get("select distinct ringer from `samtale` where CURDATE() =  DATE(datetime)").size,
-            lokaleGodkjennere = personRepository.count("groupID=${GroupID.LokalGodkjenner.nr}").toInt(),
-            avvisteRingere = personRepository.count("groupID=${GroupID.AvslaattRinger.nr}").toInt(),
+            lokaleGodkjennere = get("select 1 FROM person WHERE groupID=${GroupID.LokalGodkjenner.nr}").size,
+            avvisteRingere = get("select 1 FROM person WHERE groupID=${GroupID.AvslaattRinger.nr}").size,
             antallLokallagRingtFraTotalt = get("select distinct ringer from `samtale` c " +
                     "inner join ringer ringer on c.ringer = ringer.id " +
                     "inner join person p on ringer.personId = p.id " +
@@ -63,9 +55,9 @@ class StatistikkService(
         )
 
     private fun getPersonerStatistikkResponse(): PersonerStatistikkResponse = PersonerStatistikkResponse(
-        antallPersonerISystemetTotalt = personRepository.count().toInt(),
-        ringere = personRepository.count("groupID>${GroupID.Slett.nr}").toInt(),
-        ferdigringte = personRepository.count("groupID=${GroupID.Ferdigringt.nr} or groupID=${GroupID.Slett.nr}").toInt(),
+        antallPersonerISystemetTotalt = get("SELECT 1 FROM person").size,
+        ringere = get("SELECT 1 FROM person WHERE groupID > ${GroupID.Slett.nr}").size, //TODO: Bruk heller eksplisitte statusar
+        ferdigringte = get("select 1 FROM person WHERE groupID=${GroupID.Ferdigringt.nr} or groupID=${GroupID.Slett.nr}").size,
         ringtUtenSvar = get("select 1 FROM person p inner join samtale s on s.ringt=p.id AND p.groupID=${GroupID.KlarTilAaRinges.nr}").size,
         ikkeRingt = get("select p.* FROM person p where p.groupID < ${GroupID.UgodkjentRinger.nr} and not exists (select 1 from samtale s where s.ringt=p.id)").size,
         antallLokallagMedPersonerTilknytta = get( "select distinct lokallag FROM person where lokallag is not null").size
