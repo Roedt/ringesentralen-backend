8ae28811d22bfc2ae5245c94c9808b18264ab966
Mads Opheim <mads@fostad.net>
2021-04-15 21:04:02 +0200

    Rettar syntaksfeil i database-utfylling
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- sql/init/1_generer_databasen.sql
+++ sql/init/1_generer_databasen.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `hibernate_sequence`(
     primary key (sequence_name)
 ) engine=MyISAM;
 
-insert into `hibernate_sequence` (`next_val`) values(26);
+insert into `hibernate_sequence` (`next_val`) values(50);
 
 -- --------------------------------------------------------
 
@@ -6310,36 +6310,36 @@ INSERT INTO `ringer` (`personId`) VALUES
 
 
 INSERT INTO `person` (`fornavn`, `etternavn`, `telefonnummer`, `postnummer`, `email`, `fylke`, `groupID`, `oppretta`, `lokallag`, `kilde`) VALUES
-('Telefonnummer som ikke', 'Er inne i systemet', '-1',	-1,	NULL,	-1,	1,	'2020-08-22 23:29:09', -1, 'Systembruker'),
+('Telefonnummer som ikke', 'Er inne i systemet', '-1',	-1,	NULL,	-1,	1,	'2020-08-22 23:29:09', -1, 'Systembruker');
 
 
-INSERT INTO `person` (`fornavn`, `etternavn`, `telefonnummer`, `postnummer`, `email`, `fylke`, `groupID`, `oppretta`, `lokallag`) VALUES
-('Donald', ' Duck',	'+4712345678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 209),
-('Hetti', ' Duck',	'+4712345677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 209),
-('Letti', ' Duck',	'+4712345679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Netti', ' Duck',	'+4712345676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Klodrik', ' Duck',	'+4712345675', 1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Anton', ' Duck',	'+4712345674',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Bestemor', ' Duck',	'+4712345673',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Skrue', 'McDuck',	'+4712345672',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1),
-('Gulbrand', 'Gråstein',	'+4712345671',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1),
-('Spøkelseskladden', '',	'+4712345670',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1);
+INSERT INTO `person` (`fornavn`, `etternavn`, `telefonnummer`, `postnummer`, `email`, `fylke`, `groupID`, `oppretta`, `lokallag`, `kilde`) VALUES
+('Donald', ' Duck',	'+4712345678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 209, 'Verva'),
+('Hetti', ' Duck',	'+4712345677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 209, 'Verva'),
+('Letti', ' Duck',	'+4712345679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Netti', ' Duck',	'+4712345676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Klodrik', ' Duck',	'+4712345675', 1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Anton', ' Duck',	'+4712345674',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Bestemor', ' Duck',	'+4712345673',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Skrue', 'McDuck',	'+4712345672',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Gulbrand', 'Gråstein',	'+4712345671',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Spøkelseskladden', '',	'+4712345670',	3050,	NULL,	6,	1,	'2020-08-22 23:29:09', 1, 'Verva');
 
-INSERT INTO `person` (`fornavn`, `etternavn`, `telefonnummer`, `postnummer`, `email`, `fylke`, `groupID`, `oppretta`, `lokallag`) VALUES
-('Aster', 'ix',	'+4722345678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Obel', 'ix',	'+4722345677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Idef', 'ix',	'+4722345679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Majest', 'ix',	'+4722345676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Miracul', 'ix',	'+4722345675',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Hermet', 'ix',	'+4722345665',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Trubadur', 'ix',	'+4722346676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Barometr', 'ix',	'+4722346677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Gode', 'mine',	'+4722346678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Senil', 'ix',	'+4722346679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Armam', 'ix',	'+4722345680',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Lillef', 'ix',	'+4722345681',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Remoul', 'adine',	'+4722345682',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1),
-('Tragicom', 'ix',	'+4722345683',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1);
+INSERT INTO `person` (`fornavn`, `etternavn`, `telefonnummer`, `postnummer`, `email`, `fylke`, `groupID`, `oppretta`, `lokallag`, `kilde`) VALUES
+('Aster', 'ix',	'+4722345678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Obel', 'ix',	'+4722345677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Idef', 'ix',	'+4722345679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Majest', 'ix',	'+4722345676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Miracul', 'ix',	'+4722345675',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Hermet', 'ix',	'+4722345665',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Trubadur', 'ix',	'+4722346676',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Barometr', 'ix',	'+4722346677',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Gode', 'mine',	'+4722346678',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Senil', 'ix',	'+4722346679',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Armam', 'ix',	'+4722345680',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Lillef', 'ix',	'+4722345681',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Remoul', 'adine',	'+4722345682',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva'),
+('Tragicom', 'ix',	'+4722345683',	1,	NULL,	-1,	1,	'2020-08-22 23:29:09', 1, 'Verva');
 
 -- --------------------------------------------------------
 
