4b433c21fbe0fd766aca209b2061874f508e8eba
Mads Opheim <mads@fostad.net>
2021-05-03 22:25:36 +0200

    Fjernar unødig jwt-krav for /ping
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/kotlin/no/roedt/ringesentralen/PingController.kt
+++ src/main/kotlin/no/roedt/ringesentralen/PingController.kt
@@ -1,7 +1,6 @@
 package no.roedt.ringesentralen
 
 import org.eclipse.microprofile.openapi.annotations.Operation
-import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import javax.annotation.security.PermitAll
 import javax.ws.rs.GET
@@ -11,7 +10,6 @@ import javax.ws.rs.core.Response
 
 @Path("/ping")
 @Tag(name = "Ping")
-@SecurityRequirement(name = "jwt")
 class PingController {
 
     @PermitAll
