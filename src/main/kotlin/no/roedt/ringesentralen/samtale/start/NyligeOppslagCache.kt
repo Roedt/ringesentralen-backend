ac2192abc2c83c5443c8c3268769c4a5421a611a
Mads Opheim <mads@fostad.net>
2021-05-07 23:12:32 +0200

    #260: Forenklar kode
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/kotlin/no/roedt/ringesentralen/samtale/start/NyligeOppslagCache.kt
+++ src/main/kotlin/no/roedt/ringesentralen/samtale/start/NyligeOppslagCache.kt
@@ -3,24 +3,22 @@ package no.roedt.ringesentralen.samtale.start
 import java.util.concurrent.ConcurrentHashMap
 import javax.enterprise.context.ApplicationScoped
 
+const val cacheTimeValidityInMillis: Long = 5000
+
 @ApplicationScoped
 class NyligeOppslagCache {
-    private var cacheTimeValidityInMillis: Long = 5000
     private val hashMap = ConcurrentHashMap<Int, TimedEntry>()
 
     fun assertAtIngenAndreSoekerIDetteLagetAkkuratNo(lokallag: Int) {
-        var i = 0
         while (hashMap.containsKey(lokallag) && hashMap[lokallag]!!.isGyldig()) {
-            Thread.sleep(100);
-            i++
+            Thread.sleep(100)
         }
-        hashMap[lokallag] = TimedEntry(cacheTimeValidityInMillis)
+        hashMap[lokallag] = TimedEntry()
     }
 
     fun remove(lokallag: Int) = hashMap.remove(lokallag)
 
-    data class TimedEntry(val maxDurationInMillis: Long) {
-        private val creationTime: Long = System.currentTimeMillis()
-        fun isGyldig() = (System.currentTimeMillis() - creationTime) <= maxDurationInMillis
+    data class TimedEntry(val creationTime: Long = System.currentTimeMillis()) {
+        fun isGyldig() = (System.currentTimeMillis() - creationTime) <= cacheTimeValidityInMillis
     }
 }
