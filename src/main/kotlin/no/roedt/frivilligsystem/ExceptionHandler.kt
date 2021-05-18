ff81ff5ac5aabfcfeafe7cf7f313a605ec2cfea9
Mads Opheim <mads@fostad.net>
2021-05-18 20:15:45 +0200

    #272: Loggar alle json-parsing-feilmeldingar eksplisitt i backend
    Signed-off-by: Mads Opheim <mads@fostad.net>

---
+++ src/main/kotlin/no/roedt/frivilligsystem/ExceptionHandler.kt
@@ -0,0 +1,14 @@
+package no.roedt.frivilligsystem
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import javax.ws.rs.core.Response
+import javax.ws.rs.ext.ExceptionMapper
+import javax.ws.rs.ext.Provider
+
+@Provider
+class ExceptionHandler : ExceptionMapper<JsonProcessingException> {
+    override fun toResponse(exception: JsonProcessingException): Response {
+        exception.printStackTrace()
+        return Response.status(Response.Status.BAD_REQUEST).entity(exception.message).build()
+    }
+}
