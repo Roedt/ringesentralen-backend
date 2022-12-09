1f5ec307c108f238711ab5e84cabd54bf65ed853
Mads Opheim <mads@fostad.net>
2022-12-09 15:52:38 +0100

    Revert "Ser kva som skjer med firebase-admin-apiet"
    This reverts commit 5f33f71ca677cf5b39897d41d17da7d0a81cf168.

--- src/main/kotlin/no/roedt/forum/database/FirestoreCollections.kt
+++ src/main/kotlin/no/roedt/forum/database/FirestoreCollections.kt
@@ -1,25 +1,20 @@
 package no.roedt.forum.database
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.cloud.FirestoreClient
+import com.google.cloud.firestore.Firestore
 import org.eclipse.microprofile.config.inject.ConfigProperty
 import javax.annotation.PostConstruct
 import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
 
 @ApplicationScoped
 class FirestoreCollections(
     @ConfigProperty(name = "brukHypersys", defaultValue = "true")
-    private val brukHypersys: Boolean,
-
-    @ConfigProperty(name = "projectId")
-    private val projectId: String
+    private val brukHypersys: Boolean
 ) {
     var collections: List<FirestoreCollection>? = null
 
-//    @Inject
-//    private lateinit var firestore: Firestore
+    @Inject
+    private lateinit var firestore: Firestore
 
     @PostConstruct
     fun setup() {
@@ -27,18 +22,7 @@ class FirestoreCollections(
             collections = lagFakeCollections()
             return
         }
-//        collections = firestore.listCollections()
-//            .map { RealFirestoreCollection(underforumnavn = it.id, collectionReference = it) }
-//            .toList()
-        val credentials = GoogleCredentials.getApplicationDefault()
-        val options = FirebaseOptions.Builder()
-            .setCredentials(credentials)
-            .setProjectId(projectId)
-            .build()
-        FirebaseApp.initializeApp(options)
-
-        val db = FirestoreClient.getFirestore()
-        collections = db.listCollections()
+        collections = firestore.listCollections()
             .map { RealFirestoreCollection(underforumnavn = it.id, collectionReference = it) }
             .toList()
     }
