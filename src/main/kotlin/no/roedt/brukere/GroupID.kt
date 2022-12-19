49dfc65ebb54c738f63b89cdcefcde24d5153b78
Mads Opheim <mads@fostad.net>
2022-12-19 10:44:18 +0100

    Slettar ubrukt mappingkode

--- src/main/kotlin/no/roedt/brukere/GroupID.kt
+++ src/main/kotlin/no/roedt/brukere/GroupID.kt
@@ -6,7 +6,6 @@ interface GroupID {
     val skildring: String
     val roller: Set<String>
     fun references(value: Int): Boolean = nr == value
-    fun oneOf(vararg value: Int): Boolean = value.contains(nr)
 
     companion object {
         fun referencesOneOf(groupID: Int, vararg groupIDs: GroupID) = groupIDs.map { it.nr }.any { it == groupID }
