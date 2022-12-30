a32275e27a299e6ae87b5da03d613b0aca65a793
Mads Opheim <mads@fostad.net>
2022-12-30 15:04:48 +0100

    Rettar så vi kan hente oppdaterte roller både som eksisterande brukar og som ein som ventar på innlogging. Gjer med det samme om til get, her gjer vi jo berre henting av data

--- src/main/kotlin/no/roedt/innloggaBruker/InnloggaBrukerController.kt
+++ src/main/kotlin/no/roedt/innloggaBruker/InnloggaBrukerController.kt
@@ -10,7 +10,6 @@ import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import javax.annotation.security.RolesAllowed
 import javax.ws.rs.GET
-import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.Context
@@ -41,11 +40,11 @@ class InnloggaBrukerController(val innloggaBrukerService: InnloggaBrukerService,
     @Retry
     fun getLokallag(@Context ctx: SecurityContext) = innloggaBrukerService.getLokallag(ctx.userId(), jwt.groups)
 
-    @RolesAllowed(GenerellRolle.bruker)
-    @POST
+    @RolesAllowed(GenerellRolle.venterPaaGodkjenning, GenerellRolle.bruker)
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/roller")
-    @Operation(summary = "Hent mine oppdaterte roller", description = GenerellRolle.bruker)
+    @Operation(summary = "Hent mine oppdaterte roller", description = GenerellRolle.brukerVenterPaaGodkjenning)
     @Bulkhead(3)
     @Retry
     fun hentOppdaterteRoller(@Context ctx: SecurityContext) = innloggaBrukerService.getRoller(ctx.userId())
