0f05141a5341db2cbf99c93ae1e2d8309f09035f
Mads Opheim <mads@fostad.net>
2023-01-03 11:58:32 +0100

    Tar bort igjen serialiseringsfiks, vart enklare og betre med eksplisitt DTO-objekt

--- src/main/kotlin/no/roedt/person/Person.kt
+++ src/main/kotlin/no/roedt/person/Person.kt
@@ -1,14 +1,5 @@
 package no.roedt.person
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import io.quarkus.runtime.annotations.RegisterForReflection
 import no.roedt.Kilde
 import no.roedt.Kommune
@@ -16,7 +7,6 @@ import no.roedt.RoedtPanacheEntity
 import no.roedt.brukere.GroupID
 import no.roedt.ringesentralen.brukere.RingesentralenGroupID
 import java.time.Instant
-import javax.enterprise.inject.spi.CDI
 import javax.persistence.Basic
 import javax.persistence.Cacheable
 import javax.persistence.Entity
@@ -39,8 +29,6 @@ data class Person(
     var email: String?,
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "postnummer", nullable = false)
-    @JsonSerialize(using = PostnummerSerializer::class)
-    @JsonDeserialize(using = PostnummerDeserializer::class)
     var postnummer: Postnummer,
     var fylke: Int,
     var lokallag: Int,
@@ -78,21 +66,4 @@ data class Person(
     }
 
     fun setGroupID(groupID: GroupID) = setGroupID(groupID.nr)
-}
-
-object PostnummerSerializer : JsonSerializer<Postnummer>() {
-    override fun serialize(postnummer: Postnummer, gen: JsonGenerator, serializers: SerializerProvider) {
-        with(gen) {
-            writeString(postnummer.Postnummer)
-        }
-    }
-}
-
-object PostnummerDeserializer : JsonDeserializer<Postnummer>() {
-    override fun deserialize(parser: JsonParser, context: DeserializationContext): Postnummer {
-        val repo = CDI.current().select(PostnummerRepository::class.java).get()
-        val tree: JsonNode = parser.codec.readTree(parser)
-        val postnr = tree.get("postnummer").textValue()
-        return repo.findById(postnr)
-    }
 }
