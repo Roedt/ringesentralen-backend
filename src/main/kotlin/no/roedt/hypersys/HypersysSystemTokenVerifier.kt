6a9d29b1eed3cfb566106e886418d91647cbf3b4
Mads Opheim <mads@fostad.net>
2023-01-04 17:33:42 +0100

    Fixes #372: Tar høgde for at Hypersys kan vera nede og let ikkje det hindre serveroppstart

--- src/main/kotlin/no/roedt/hypersys/HypersysSystemTokenVerifier.kt
+++ src/main/kotlin/no/roedt/hypersys/HypersysSystemTokenVerifier.kt
@@ -1,6 +1,8 @@
 package no.roedt.hypersys
 
 import no.roedt.token.SecretFactory
+import org.eclipse.microprofile.faulttolerance.Fallback
+import org.eclipse.microprofile.faulttolerance.Timeout
 import javax.annotation.PostConstruct
 import javax.enterprise.context.ApplicationScoped
 
@@ -16,6 +18,8 @@ class HypersysSystemTokenVerifier(
         token = getTokenFromHypersys()
     }
 
+    @Timeout(value = 10000L)
+    @Fallback(fallbackMethod = "settTokenUgyldig")
     fun getTokenFromHypersys(): Token {
         val response = hypersysProxy.post(secretFactory.getHypersysClientId(), secretFactory.getHypersysClientSecret(), "grant_type=client_credentials", loggingtekst = "systeminnlogging")
         return if (response.statusCode() != 200) {
@@ -24,6 +28,8 @@ class HypersysSystemTokenVerifier(
             hypersysProxy.readResponse(response, GyldigSystemToken::class.java)
         }
     }
+
+    private fun settTokenUgyldig(): Token = UgyldigToken(error = "Kunne ikke hente token fra Hypersys")
 
     fun assertGyldigSystemToken(): GyldigSystemToken {
         if (token is UgyldigToken) throw RuntimeException("Ugyldig token $token")
--- src/main/kotlin/no/roedt/lokallag/Lokallagsynkroniserer.kt
+++ src/main/kotlin/no/roedt/lokallag/Lokallagsynkroniserer.kt
@@ -2,11 +2,16 @@ package no.roedt.lokallag
 
 import io.quarkus.runtime.StartupEvent
 import no.roedt.hypersys.HypersysService
+import org.eclipse.microprofile.faulttolerance.Fallback
+import org.eclipse.microprofile.faulttolerance.Timeout
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.event.Observes
 
 @ApplicationScoped
 class Lokallagsynkroniserer(val hypersysService: HypersysService, val lokallagRepository: LokallagRepository) {
+
+    @Timeout(value = 10000L)
+    @Fallback(fallbackMethod = "hoppOverSynkronisering")
     fun onStart(@Observes event: StartupEvent) {
         val alleLokallagFraHypersys = hypersysService.getAlleLokallag()
         val alleLokallagISystemet = lokallagRepository.listAll()
@@ -17,7 +22,18 @@ class Lokallagsynkroniserer(val hypersysService: HypersysService, val lokallagRe
             println(iHypersysMenIkkeIDatabasen)
         }
         val forventaUtanFylke =
-            setOf("Faglig Utvalg", "Hele Norge", "Internasjonalt utvalg", "Kvinneutvalg", "Miljøpolitisk utvalg", "Rød Ungdom", "Rødt", "Udefinert", "Utland", "testlaget")
+            setOf(
+                "Faglig Utvalg",
+                "Hele Norge",
+                "Internasjonalt utvalg",
+                "Kvinneutvalg",
+                "Miljøpolitisk utvalg",
+                "Rød Ungdom",
+                "Rødt",
+                "Udefinert",
+                "Utland",
+                "testlaget"
+            )
         val iDatabasenUtanFylke = alleLokallagISystemet
             .filter { it.fylke == -1 }
             .map { it.navn }
@@ -26,5 +42,9 @@ class Lokallagsynkroniserer(val hypersysService: HypersysService, val lokallagRe
             println("Lokallag som er i databasen utan fylke:")
             println(iDatabasenUtanFylke)
         }
+    }
+
+    private fun hoppOverSynkronisering() {
+        println("Oppkopling mot Hypersys tok for lang tid, så hoppar over å sjekke om alle lokallag er med.")
     }
 }
