840c33721bf551235f324b0bd5f7f19ed238fa41
Mads Opheim <mads@fostad.net>
2023-05-02 17:17:16 +0200

    SMS til mottaker har jo også gått til frivillige, så da er det ein framandnønkkel deri
    Signed-off-by: Mads Opheim <mads@fostad.net>

--- src/main/resources/db/migration/V15__fjern_gamle_frivillige.sql
+++ src/main/resources/db/migration/V15__fjern_gamle_frivillige.sql
@@ -1,5 +1,6 @@
 DELETE FROM aktivitetForFrivillig;
 DELETE FROM frivilligKorona;
 DELETE FROM frivilligOpptattAv;
+DELETE FROM smsTilMottaker;
 DELETE FROM frivillig;
 DELETE FROM person where kilde = 'Frivillig';
