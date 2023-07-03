9157d7241fcd8c5d447e1d673fdb04a1876d1ef1
Mads Opheim <mads@fostad.net>
2023-07-03 22:13:12 +0200

    Legg inn lokallag som fins i hypersys, men ikkje i databasen

---
+++ src/main/resources/db/migration/V19__nyelokallag.sql
@@ -0,0 +1,7 @@
+INSERT INTO `lokallag`(`id`, `navn`, `hypersysID`, `fylke`)
+VALUES
+    (53954,'Lier',100754,6),
+    (53955,'Raudt Suldal',100742,11),
+    (53956,'Rødt Bø i Vesterålen',100753,18),
+    (53957,'Rødt Sauda',100752,11),
+    (53958,'Rødt Sokndal',100755,11);
