72bbd8fe71a840261fa8a459e3bc77435933177e
Mads Opheim <mads@fostad.net>
2023-07-03 21:34:39 +0200

    Meir reflection-greier

--- src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
+++ src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
@@ -4,5 +4,5 @@ import com.twilio.converter.CurrencyDeserializer
 import com.twilio.rest.api.v2010.account.Message
 import io.quarkus.runtime.annotations.RegisterForReflection
 
-@RegisterForReflection(targets = [Message::class, CurrencyDeserializer::class])
+@RegisterForReflection(targets = [Message::class, CurrencyDeserializer::class, Message.Direction::class])
 class TwilioRegisterForReflection
