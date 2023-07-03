f5b74582a4d6ea53a7c09957957f2848deba02b4
Mads Opheim <mads@fostad.net>
2023-07-03 21:21:37 +0200

    Enda ei Twilio-klasse som treng ordentleg deserialisering

--- src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
+++ src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
@@ -1,7 +1,8 @@
 package no.roedt.ringesentralen.twilio
 
+import com.twilio.converter.CurrencyDeserializer
 import com.twilio.rest.api.v2010.account.Message
 import io.quarkus.runtime.annotations.RegisterForReflection
 
-@RegisterForReflection(targets = [Message::class])
+@RegisterForReflection(targets = [Message::class, CurrencyDeserializer::class])
 class TwilioRegisterForReflection
