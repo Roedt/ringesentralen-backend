b80db97f7e993877f1f8fdcc897a4aebe39d1efd
Mads Opheim <mads@fostad.net>
2023-07-03 21:38:08 +0200

    Meir reflection og twilio

--- src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
+++ src/main/kotlin/no/roedt/ringesentralen/twilio/TwilioRegisterForReflection.kt
@@ -2,7 +2,20 @@ package no.roedt.ringesentralen.twilio
 
 import com.twilio.converter.CurrencyDeserializer
 import com.twilio.rest.api.v2010.account.Message
+import com.twilio.rest.api.v2010.account.Message.AddressRetention
+import com.twilio.rest.api.v2010.account.Message.UpdateStatus
 import io.quarkus.runtime.annotations.RegisterForReflection
 
-@RegisterForReflection(targets = [Message::class, CurrencyDeserializer::class, Message.Direction::class])
+@RegisterForReflection(
+    targets = [
+        Message::class,
+        CurrencyDeserializer::class,
+        Message.Direction::class,
+        Message.ContentRetention::class,
+        AddressRetention::class,
+        UpdateStatus::class,
+        Message.Status::class,
+        Message.ScheduleType::class
+    ]
+)
 class TwilioRegisterForReflection
