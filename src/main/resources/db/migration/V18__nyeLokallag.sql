ed8bc1600b8cd6b7594524173609e31706c533a8
Mads Opheim <mads@fostad.net>
2023-07-03 21:26:11 +0200

    Oppdaterer med fylker for nye lokallag

---
+++ src/main/resources/db/migration/V18__nyeLokallag.sql
@@ -0,0 +1,9 @@
+UPDATE lokallag SET fylke = 11 where navn = 'Bjerkreim';
+UPDATE lokallag SET fylke = 11 where navn = 'Eigersund';
+UPDATE lokallag SET fylke = 11 where navn = 'Gjesdal';
+UPDATE lokallag SET fylke = 11 where navn = 'Suldal';
+UPDATE lokallag SET fylke = 4 where navn = 'Løten';
+UPDATE lokallag SET fylke = 2 where navn = 'Rødt Jevnaker';
+UPDATE lokallag SET fylke = 4 where navn = 'Rødt Rendalen';
+UPDATE lokallag SET fylke = 8 where navn = 'Rødt Færder';
+UPDATE lokallag SET fylke = 11 where navn = 'Sauda';
