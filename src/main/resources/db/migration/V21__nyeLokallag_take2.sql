9ae04e623b5cca50d46ac5a7f05e0c1494c10fc6
Mads Opheim <mads@fostad.net>
2023-07-24 21:47:04 +0200

    Nytt forsøk på å få inn nye lokallag

---
+++ src/main/resources/db/migration/V21__nyeLokallag_take2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `lokallag`(`id`, `navn`, `hypersysID`, `fylke`)
+VALUES
+    (53954,'Lier',100754,6),
+    (53956,'Rødt Bø i Vesterålen',100753,18),
+    (53958,'Rødt Sokndal',100755,11);
