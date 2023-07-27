98bd09a0e5a44b215f55e154939edeb6ffcdc4a8
Mads Opheim <mads@fostad.net>
2023-07-27 16:15:42 +0200

    #415: Lager spesifikke spørringar der dei høyrer heime i staden for å eksponere entitymanager rundt i heile basen

--- src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkRepository.kt
+++ src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkRepository.kt
@@ -3,7 +3,10 @@ package no.roedt.ringesentralen.statistikk
 import jakarta.enterprise.context.Dependent
 import jakarta.persistence.EntityManager
 import no.roedt.list
+import no.roedt.ringesentralen.brukere.RingesentralenGroupID
 import no.roedt.ringesentralen.samtale.resultat.Resultat
+import java.sql.Timestamp
+import java.time.Instant
 
 @Dependent
 class StatistikkRepository(internal val entityManager: EntityManager) {
@@ -24,4 +27,94 @@ class StatistikkRepository(internal val entityManager: EntityManager) {
             samtalerMedResultatSaaLangt = entityManager.list("SELECT ringer FROM `samtale` WHERE resultat!=${Resultat.Samtale_startet.nr}").size
         )
     }
+
+    fun getRingereStatistikkResponse(): RingereStatistikkResponse =
+        RingereStatistikkResponse(
+            registrerteRingere = entityManager.list("SELECT 1 FROM ringer").size,
+            antallSomHarRingt = entityManager.list("select distinct ringer from `samtale`").size,
+            aktiveRingereDenSisteTimen = entityManager.list("select distinct ringer from `samtale` where UNIX_TIMESTAMP(now()) - unix_timestamp(datetime) < 3600").size,
+            aktiveRingereIDag = entityManager.list("select distinct ringer from `samtale` where CURDATE() =  DATE(datetime)").size,
+            lokaleGodkjennere = entityManager.list("select 1 FROM person WHERE groupID=${RingesentralenGroupID.LokalGodkjenner.nr}").size,
+            avvisteRingere = entityManager.list("select 1 FROM person WHERE groupID=${RingesentralenGroupID.AvslaattRinger.nr}").size,
+            antallLokallagRingtFraTotalt = entityManager.list(
+                "select distinct ringer from `samtale` c " +
+                    "inner join ringer ringer on c.ringer = ringer.id " +
+                    "inner join person p on ringer.personId = p.id " +
+                    "inner join lokallag l on l.id = p.lokallag"
+            ).size
+        )
+
+    fun getPersonerStatistikkResponse(): PersonerStatistikkResponse = PersonerStatistikkResponse(
+        antallPersonerISystemetTotalt = entityManager.list("SELECT 1 FROM person").size,
+        ringere = entityManager.list("SELECT 1 FROM person WHERE groupID in (${RingesentralenGroupID.GodkjentRinger.nr}, ${RingesentralenGroupID.GodkjentRingerMedlemmer.nr}, ${RingesentralenGroupID.LokalGodkjenner.nr}, ${RingesentralenGroupID.Admin.nr} )").size,
+        ferdigringte = entityManager.list("select 1 FROM person WHERE groupID=${RingesentralenGroupID.Ferdigringt.nr} or groupID=${RingesentralenGroupID.Slett.nr}").size,
+        ringtUtenSvar = entityManager.list("select 1 FROM person p inner join samtale s on s.ringt=p.id AND p.groupID=${RingesentralenGroupID.KlarTilAaRinges.nr}").size,
+        ikkeRingt = entityManager.list("select 1 FROM person p where p.groupID < ${RingesentralenGroupID.UgodkjentRinger.nr} and not exists (select 1 from samtale s where s.ringt=p.id)").size,
+        antallLokallagMedPersonerTilknytta = entityManager.list("select distinct lokallag FROM person where lokallag is not null").size
+    )
+
+    internal fun mineRingte(hypersysId: Int): Int = entityManager.list(
+        """SELECT 1 FROM samtale samtale 
+                INNER JOIN ringer ringer on samtale.ringer=ringer.id 
+                INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId 
+                WHERE ringerPerson.hypersysID=$hypersysId 
+                and samtale.resultat != ${Resultat.Samtale_startet.nr} 
+                and samtale.ringt != ringerPerson.id
+        """
+    ).size
+
+    internal fun personSomHarRingtFlest(): Int = entityManager.list(
+        """SELECT count(1) FROM samtale samtale 
+            INNER JOIN ringer ringer on samtale.ringer=ringer.id 
+            INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId
+            WHERE samtale.resultat != 9
+            and samtale.ringt != ringerPerson.id
+            group by ringer.id
+            order by count(1) desc limit 1
+        """
+    )[0].toString().toInt()
+
+    internal fun samtalerGjennomfoertILaget(hypersysId: Int): Int =
+        entityManager.list(
+            """SELECT 1 FROM samtale samtale 
+                INNER JOIN ringer ringer on samtale.ringer=ringer.id 
+                INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId            
+                WHERE ringerPerson.lokallag=(select lokallag from person where hypersysID=$hypersysId) 
+                and samtale.resultat != ${Resultat.Samtale_startet.nr} 
+                and samtale.ringt != ringerPerson.id
+            """
+        ).size
+
+    internal fun antallRingereILaget(hypersysId: Int): Int =
+        entityManager.list(
+            """SELECT 1 FROM ringer ringer
+                INNER JOIN person person on ringer.personId = person.id
+                WHERE person.lokallag =(select lokallag from person where hypersysID=$hypersysId)
+            """
+        ).size
+
+
+    fun lodd(fra: Instant, til: Instant): List<LoddStatistikk> =
+        entityManager.list(
+            """
+                SELECT ringerPerson.fornavn, ringerPerson.etternavn, lokallag.navn, count(1)
+                FROM samtale samtale
+                INNER JOIN ringer ringer on ringer.id = samtale.ringer
+                INNER JOIN person ringerPerson on ringer.personId = ringerPerson.id
+                INNER JOIN lokallag lokallag on lokallag.id = ringerPerson.lokallag
+                AND samtale.datetime >= '${Timestamp.from(fra)}'
+                AND samtale.datetime <= '${Timestamp.from(til)}'
+                AND samtale.resultat != ${Resultat.Samtale_startet.nr}
+                GROUP BY ringerPerson.id
+            """.trimIndent()
+        )
+            .map { it as Array<*> }
+            .map {
+                LoddStatistikk(
+                    fornavn = it[0].toString(),
+                    etternavn = it[1].toString(),
+                    lokallag = it[2].toString(),
+                    antallSamtaler = it[3].toString().toInt()
+                )
+            }
 }
--- src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkService.kt
+++ src/main/kotlin/no/roedt/ringesentralen/statistikk/StatistikkService.kt
@@ -1,129 +1,34 @@
 package no.roedt.ringesentralen.statistikk
 
 import jakarta.enterprise.context.ApplicationScoped
-import no.roedt.DatabaseUpdater
 import no.roedt.brukere.GenerellRolle
-import no.roedt.ringesentralen.brukere.RingesentralenGroupID
-import no.roedt.ringesentralen.samtale.resultat.Resultat
-import java.sql.Timestamp
 import java.time.Instant
 
 @ApplicationScoped
-class StatistikkService(val databaseUpdater: DatabaseUpdater, val repository: StatistikkRepository) {
+class StatistikkService(val repository: StatistikkRepository) {
 
     fun getStatistikk(groups: Set<String>): StatistikkResponse {
         return if (groups.contains(GenerellRolle.admin)) {
             StatistikkResponse(
-                samtalerStatistikkResponse = getSamtalerStatistikkResponse(),
-                ringereStatistikkResponse = getRingereStatistikkResponse(),
-                personerStatistikkResponse = getPersonerStatistikkResponse()
+                samtalerStatistikkResponse = repository.getSamtalerStatistikkResponse(),
+                ringereStatistikkResponse = repository.getRingereStatistikkResponse(),
+                personerStatistikkResponse = repository.getPersonerStatistikkResponse()
             )
         } else {
             StatistikkResponse(
-                samtalerStatistikkResponse = getSamtalerStatistikkResponse(),
+                samtalerStatistikkResponse = repository.getSamtalerStatistikkResponse(),
                 ringereStatistikkResponse = null,
                 personerStatistikkResponse = null
             )
         }
     }
 
-    private fun getSamtalerStatistikkResponse() = repository.getSamtalerStatistikkResponse()
-
-    private fun getRingereStatistikkResponse(): RingereStatistikkResponse =
-        RingereStatistikkResponse(
-            registrerteRingere = get("SELECT 1 FROM ringer").size,
-            antallSomHarRingt = get("select distinct ringer from `samtale`").size,
-            aktiveRingereDenSisteTimen = get("select distinct ringer from `samtale` where UNIX_TIMESTAMP(now()) - unix_timestamp(datetime) < 3600").size,
-            aktiveRingereIDag = get("select distinct ringer from `samtale` where CURDATE() =  DATE(datetime)").size,
-            lokaleGodkjennere = get("select 1 FROM person WHERE groupID=${RingesentralenGroupID.LokalGodkjenner.nr}").size,
-            avvisteRingere = get("select 1 FROM person WHERE groupID=${RingesentralenGroupID.AvslaattRinger.nr}").size,
-            antallLokallagRingtFraTotalt = get(
-                "select distinct ringer from `samtale` c " +
-                    "inner join ringer ringer on c.ringer = ringer.id " +
-                    "inner join person p on ringer.personId = p.id " +
-                    "inner join lokallag l on l.id = p.lokallag"
-            ).size
-        )
-
-    private fun getPersonerStatistikkResponse(): PersonerStatistikkResponse = PersonerStatistikkResponse(
-        antallPersonerISystemetTotalt = get("SELECT 1 FROM person").size,
-        ringere = get("SELECT 1 FROM person WHERE groupID in (${RingesentralenGroupID.GodkjentRinger.nr}, ${RingesentralenGroupID.GodkjentRingerMedlemmer.nr}, ${RingesentralenGroupID.LokalGodkjenner.nr}, ${RingesentralenGroupID.Admin.nr} )").size,
-        ferdigringte = get("select 1 FROM person WHERE groupID=${RingesentralenGroupID.Ferdigringt.nr} or groupID=${RingesentralenGroupID.Slett.nr}").size,
-        ringtUtenSvar = get("select 1 FROM person p inner join samtale s on s.ringt=p.id AND p.groupID=${RingesentralenGroupID.KlarTilAaRinges.nr}").size,
-        ikkeRingt = get("select 1 FROM person p where p.groupID < ${RingesentralenGroupID.UgodkjentRinger.nr} and not exists (select 1 from samtale s where s.ringt=p.id)").size,
-        antallLokallagMedPersonerTilknytta = get("select distinct lokallag FROM person where lokallag is not null").size
-    )
-
-    private fun get(query: String) = databaseUpdater.getResultList(query)
-
     fun getRingtMest(hypersysId: Int): RingtFlestStatistikk = RingtFlestStatistikk(
-        jegHarRingt = mineRingte(hypersysId),
-        maksRingt = personSomHarRingtFlest(),
-        samtalerGjennomfoertILaget = samtalerGjennomfoertILaget(hypersysId),
-        antallRingereILaget = antallRingereILaget(hypersysId)
+        jegHarRingt = repository.mineRingte(hypersysId),
+        maksRingt = repository.personSomHarRingtFlest(),
+        samtalerGjennomfoertILaget = repository.samtalerGjennomfoertILaget(hypersysId),
+        antallRingereILaget = repository.antallRingereILaget(hypersysId)
     )
 
-    private fun mineRingte(hypersysId: Int): Int = get(
-        """SELECT 1 FROM samtale samtale 
-                INNER JOIN ringer ringer on samtale.ringer=ringer.id 
-                INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId 
-                WHERE ringerPerson.hypersysID=$hypersysId 
-                and samtale.resultat != ${Resultat.Samtale_startet.nr} 
-                and samtale.ringt != ringerPerson.id
-        """
-    ).size
-
-    private fun personSomHarRingtFlest(): Int = get(
-        """SELECT count(1) FROM samtale samtale 
-            INNER JOIN ringer ringer on samtale.ringer=ringer.id 
-            INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId
-            WHERE samtale.resultat != 9
-            and samtale.ringt != ringerPerson.id
-            group by ringer.id
-            order by count(1) desc limit 1
-        """
-    )[0].toString().toInt()
-
-    private fun samtalerGjennomfoertILaget(hypersysId: Int): Int =
-        get(
-            """SELECT 1 FROM samtale samtale 
-                INNER JOIN ringer ringer on samtale.ringer=ringer.id 
-                INNER JOIN person ringerPerson on ringerPerson.id=ringer.personId            
-                WHERE ringerPerson.lokallag=(select lokallag from person where hypersysID=$hypersysId) 
-                and samtale.resultat != ${Resultat.Samtale_startet.nr} 
-                and samtale.ringt != ringerPerson.id
-            """
-        ).size
-
-    private fun antallRingereILaget(hypersysId: Int): Int =
-        get(
-            """SELECT 1 FROM ringer ringer
-                INNER JOIN person person on ringer.personId = person.id
-                WHERE person.lokallag =(select lokallag from person where hypersysID=$hypersysId)
-            """
-        ).size
-
-    fun lodd(fra: Instant, til: Instant): List<LoddStatistikk> =
-        get(
-            """
-                SELECT ringerPerson.fornavn, ringerPerson.etternavn, lokallag.navn, count(1)
-                FROM samtale samtale
-                INNER JOIN ringer ringer on ringer.id = samtale.ringer
-                INNER JOIN person ringerPerson on ringer.personId = ringerPerson.id
-                INNER JOIN lokallag lokallag on lokallag.id = ringerPerson.lokallag
-                AND samtale.datetime >= '${Timestamp.from(fra)}'
-                AND samtale.datetime <= '${Timestamp.from(til)}'
-                AND samtale.resultat != ${Resultat.Samtale_startet.nr}
-                GROUP BY ringerPerson.id
-            """.trimIndent()
-        )
-            .map { it as Array<*> }
-            .map {
-                LoddStatistikk(
-                    fornavn = it[0].toString(),
-                    etternavn = it[1].toString(),
-                    lokallag = it[2].toString(),
-                    antallSamtaler = it[3].toString().toInt()
-                )
-            }
+    fun lodd(fra: Instant, til: Instant): List<LoddStatistikk> = repository.lodd(fra, til)
 }
