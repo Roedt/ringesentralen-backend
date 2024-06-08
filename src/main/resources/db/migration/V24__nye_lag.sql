28ef02545830aeed0252096cd08995a0fcbf08e5
Mads Opheim <mads@fostad.net>
2024-06-08 15:01:58 +0200

    Oppdaterer fylke for nye lag. Fixes #450

---
+++ src/main/resources/db/migration/V24__nye_lag.sql
@@ -0,0 +1,5 @@
+UPDATE lokallag SET fylke = 19 where navn = 'Rødt Nordreisa';
+UPDATE lokallag SET fylke = 2 where navn = 'Rødt Enebakk';
+UPDATE lokallag SET fylke = 1 where navn = 'Rødt Tjeldsund';
+UPDATE lokallag SET fylke = 3 where navn = 'Rødt Trondheim Huseby, Saupstad og Kolstad';
+UPDATE lokallag SET fylke = 4 where navn = 'Rødt Nord-Odal';
